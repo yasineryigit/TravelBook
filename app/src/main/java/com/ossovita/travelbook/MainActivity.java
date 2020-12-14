9d9595c497f3d630a40bcfc21f4c340e7dce9606
sancaryum <sancaryum@gmail.com>
2020-12-14 23:25:39 +0300

    Menu created

--- app/src/main/java/com/ossovita/travelbook/MainActivity.java
+++ app/src/main/java/com/ossovita/travelbook/MainActivity.java
@@ -1,14 +1,42 @@
 package com.ossovita.travelbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater mi = getMenuInflater();
+        mi.inflate(R.menu.add_place,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if(item.getItemId()==R.id.add_place){
+            Intent i = new Intent(this,MapsActivity.class);
+            i.putExtra("Info","new");
+            startActivity(i);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
